66d1379a82bc89e910f3e95963c0e5f038435f9d
Vikas <vikas1000dhiman@outlook.com>
2025-05-26 20:03:38 +0530

    set up initial firebase config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import { app } from './Firebase/Firebase'
+import { getDatabase, ref, set } from 'firebase/database'
 import './index.css'
+
+const db = getDatabase(app)
+
+function writeUserData() {
+  set(ref(db, 'admin'), {
+    id: Date.now(),
+    username: 'Vikas',
+    email: 'vikas1000dhiman@gmail.com',
+  })
+}
 
 function App() {
   return (
@@ -7,6 +19,12 @@ function App() {
         <p className='text-4xl font-bold underline bg-green-500'>
           Click on the Vite and React logos to learn more
         </p>
+        <button
+          onClick={() => writeUserData()}
+          className='cursor-pointer p-2 text-gray-400 m-4 border-none rounded-md bg-blue-600'
+        >
+          Put data
+        </button>
       </div>
     </>
   )
