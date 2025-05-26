7b79005197bc2a6ac8476145d36f168c6be1c90e
Vikas <vikas1000dhiman@outlook.com>
2025-05-26 18:05:54 +0530

    Initial setup Install react, tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className='text-4xl font-bold underline bg-green-500'>
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
